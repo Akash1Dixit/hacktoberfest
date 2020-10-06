a3b6e26bef012cec0a8eb1e74ec5bf55cfd7027d
technotreasure <66162844+technotreasure@users.noreply.github.com>
2020-10-06 12:49:51 +0530

    Create Convert_Decimal_to_Octal.java

---
+++ Convert_Decimal_to_Octal.java
@@ -0,0 +1,9 @@
+//Java Program to demonstrate the use of Integer.toOctalString() method  
+public class DecimalToOctalExample1{  
+public static void main(String args[]){  
+//Using the predefined Integer.toOctalString() method  
+//to convert decimal value into octal  
+System.out.println(Integer.toOctalString(8));  
+System.out.println(Integer.toOctalString(19));  
+System.out.println(Integer.toOctalString(81));  
+}}  
